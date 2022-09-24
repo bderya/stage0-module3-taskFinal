f6434124ed0561e0e7457116a24f9ffe17be421a
Batuhan Derya <bderya@3-G-9.42heilbronn.de>
2022-09-24 18:49:58 +0200

    FirstTest

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+		int first = number / 1000;
+        int second = number / 100 % 10;
+        int third = number / 10 % 10;
+        int forth = number % 10;
+        System.out.println(third + second + first + forth);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+		int lastDigit = 0;
+		int reverse = 0;
+		while(number >=1)
+		{
+			lastDigit = number % 10;
+			reverse = reverse * 10 + lastDigit;
+			number =number / 10;
+		}
+		System.out.println(reverse);
     }
 }
